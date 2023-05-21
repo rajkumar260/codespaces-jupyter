a88c9321cae2f8b1666d5bcdcb174ffbef633a1e
rajkumar260 <134230069+rajkumar260@users.noreply.github.com>
2023-05-21 19:05:04 +0000

    first python code

---
+++ helloo.py
@@ -0,0 +1,7 @@
+fruits = ['Mango', 'Mango', 'Guava', 'Apple', 'Grapes', 'Guava']
+fruit_ = {}
+fruit_['Mango'] = fruits.count('Mango')
+fruit_['Guava'] = fruits.count('Guava')
+fruit_['Apple'] = fruits.count('Apple')
+fruit_['Grapes'] = fruits.count('Grapes')
+print(fruit_)
